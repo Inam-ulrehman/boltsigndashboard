e3e4f5a7399d294f0b69b6686ec97f230f9f9f07
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-02-14 10:50:13 -0500

    fix

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -1,5 +1,7 @@
+import EditSingleProduct from './EditSingleProduct'
 import Heading from './Heading'
 import List from './List'
 import Pagination from './Pagination'
 import UploadSingleProduct from './UploadSingleProduct'
-export { Heading, Pagination, List, UploadSingleProduct }
+
+export { Heading, Pagination, List, EditSingleProduct, UploadSingleProduct }
